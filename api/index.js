d8c90b37889d54b330480d2422075dae209fe9a2
Adeeb-06 <mohammadadeeb886@gmail.com>
2025-11-13 20:54:22 +0600

    fs

--- api/index.js
+++ api/index.js
@@ -249,4 +249,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-export default app;
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
