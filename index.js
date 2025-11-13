fbaa2a10909aed8c59a20286a3d88680421fb254
Adeeb-06 <mohammadadeeb886@gmail.com>
2025-11-13 20:28:17 +0600

    errrrrrrrr

--- index.js
+++ index.js
@@ -249,6 +249,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server is running on port ${process.env.PORT}`);
+// });
