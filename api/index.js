6bd14b1f939ea2fbd1468bf6c20805357a8ce1ff
Adeeb-06 <mohammadadeeb886@gmail.com>
2025-11-13 20:50:55 +0600

    vercel

--- api/index.js
+++ api/index.js
@@ -249,6 +249,4 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+export default app;
